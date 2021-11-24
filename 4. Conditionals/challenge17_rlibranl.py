304d38789fee15eab14cf348f9e199911dfdf09f
Rodrigo <rolibranl@hotmail.es>
2021-11-24 23:15:02 +0100

    challenges

---
+++ 4. Conditionals/challenge17_rlibranl.py
@@ -0,0 +1,8 @@
+def coin_toss_app():
+    print(f"\tWelcome to the Coin Toss App\n")
+    print(f"\tI will flip a coin a set number of times")
+
+    flips = int(input(f"\tHow many times would you like me to flip the coin:\t"))
+    ask = input(f"\tWould you like to see the result of each flip (y / n):\t")
+
+    
